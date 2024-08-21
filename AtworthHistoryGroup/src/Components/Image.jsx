a67265d8a262b0debaf63b550b71de63b97c6e92
Catherine Pulman <159820305+Clatherine@users.noreply.github.com>
2024-08-21 16:20:44 +0100

    converted images on image component to webps

--- AtworthHistoryGroup/src/Components/Image.jsx
+++ AtworthHistoryGroup/src/Components/Image.jsx
@@ -1,66 +1,73 @@
-import Carousel from 'react-bootstrap/Carousel';
-import { useState } from 'react';
+import Carousel from "react-bootstrap/Carousel";
+import { useState } from "react";
 
-function Image (){
-    const [activeIndex, setActiveIndex] = useState(0);
+function Image() {
+  const [activeIndex, setActiveIndex] = useState(0);
 
-      const handleSelect = (selectedIndex, e) => {
-          setActiveIndex(selectedIndex)}
+  const handleSelect = (selectedIndex, e) => {
+    setActiveIndex(selectedIndex);
+  };
 
-    return(
+  return (
     <section id="image">
-        <Carousel id="top-image-container" data-bs-theme="dark" indicators={false} controls= {false} interval={6000} activeIndex={activeIndex} onSelect={handleSelect} >
-          <Carousel.Item>
+      <Carousel
+        id="top-image-container"
+        data-bs-theme="dark"
+        indicators={false}
+        controls={false}
+        interval={6000}
+        activeIndex={activeIndex}
+        onSelect={handleSelect}
+      >
+        <Carousel.Item>
           <div className="carousel-photo-container">
-            <img
-           
-           className="carousel-photo"
-              src="/images/Gallery_pics/Bath Road (10).jpg" alt="The New Inn on Bath Road" 
-            />
-            </div>
-          </Carousel.Item>
-          <Carousel.Item>
+            <picture className="carousel-photo-container">
+              <source
+                srcSet="/images/Gallery_pics/Bath Road (10).webp"
+                type="image/webp"
+              />
+              <img
+                className="carousel-photo"
+                src="/images/Gallery_pics/Bath Road (10).jpg"
+                alt="The New Inn on Bath Road"
+              />
+            </picture>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
           <div className="carousel-photo-container">
-            <img
-           
-           className="carousel-photo"
-              src="/images/Gallery_pics/Bath Road (15).jpg" alt="image of Bath Road with Clock Tower in backgroun"
-            />
-             </div>
-          </Carousel.Item>
-          {/* <Carousel.Item>
+            <picture className="carousel-photo-container">
+              <source
+                srcSet="/images/Gallery_pics/Bath Road (15).webp"
+                type="image/webp"
+              />
+              <img
+                className="carousel-photo"
+                src="/images/Gallery_pics/Bath Road (15).jpg"
+                alt="image of Bath Road with Clock Tower in backgroun"
+              />
+            </picture>
+          </div>
+        </Carousel.Item>
+
+        <Carousel.Item>
           <div className="carousel-photo-container">
-            <img 
-              className="carousel-photo"
-              src="/images/Gallery_pics/Bath Road (14).jpg" alt="image of Bath Road with horse-drawn cart" 
-            />
-             </div>
-          </Carousel.Item> */}
-          <Carousel.Item>
-          <div className="carousel-photo-container">
-            <img 
-              className="carousel-photo"
-              src="/images/Gallery_pics/St Michaels (6).jpg" alt="image of St Michaels Church" 
-            />
-             </div>
-          </Carousel.Item>
-        </Carousel>
-        {/* <div className="carousel-captions">
-<div className="carousel-caption-item">
-
-{activeIndex === 0 && <p>The New Inn on Bath Road</p>}
-{activeIndex === 1 && <p>Bath Road with Clock Tower in background</p>}
-{activeIndex === 2 && <p>Bath Road with horse-drawn cart</p>}
-{activeIndex === 3 && <p>St Michaels Church</p>}
-</div>
-</div> */}
-   </section>
-
-      )
-
-    }
-
+            <picture className="carousel-photo-container">
+              <source
+                srcSet="/images/Gallery_pics/St Michaels (6).webp"
+                type="image/webp"
+              />
+              <img
+                className="carousel-photo"
+                src="/images/Gallery_pics/St Michaels (6).jpg"
+                alt="image of St Michaels Church"
+              />
+            </picture>
+          </div>
+        </Carousel.Item>
+      </Carousel>
+    </section>
+  );
+}
 
 export default Image;
-
-
