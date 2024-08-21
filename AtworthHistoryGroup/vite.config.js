46e9e8c86343a6392f676f7200becee35f274786
Catherine Pulman <159820305+Clatherine@users.noreply.github.com>
2024-08-21 16:10:33 +0100

    updated vite.config.js file to use viteImagemin

--- AtworthHistoryGroup/vite.config.js
+++ AtworthHistoryGroup/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import SitemapPlugin from 'vite-plugin-sitemap';
+import viteImagemin from 'vite-plugin-imagemin';
+import imageminWebp from 'imagemin-webp';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +11,14 @@ export default defineConfig({
     SitemapPlugin({
       hostname: 'https://atworthhistorygroup.netlify.app',
       // You can add more options here if needed
-    })
+    }),
+    viteImagemin({
+      // WebP conversion configuration
+      plugins: [
+        imageminWebp({
+          quality: 75,
+        }),
+      ],
+    }),
   ],
 });
